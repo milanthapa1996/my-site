4f11416058ca8d5379e56a15fe0dca1194836427
milanthapa1996 <tmilan0604@gmail.com>
2023-01-03 20:28:59 +0545

    update

--- app/head.jsx
+++ app/head.jsx
@@ -5,6 +5,12 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Personal portfolio of Milan Thapa" />
       <link rel="icon" href="/logo.ico" />
+
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3056250846196186"
+        crossOrigin="anonymous"
+      ></script>
     </>
-  )
+  );
 }
